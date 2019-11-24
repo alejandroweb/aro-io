082257124a553767d0bc6c990071c6ff84016a0b
Alejandro Rodriguez <hi@aro.io>
2019-11-24 18:44:55 +0100

    Updates

---
+++ precache-manifest.e16c5b7e91d528cc71a32fbbb236128e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7ace2bb0314c23e9dec962a78a443a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "249a4c26dd7edf964147",
+    "url": "/static/css/main.11a37e9b.chunk.css"
+  },
+  {
+    "revision": "9a2fbcd8c5d7ea6b78ec",
+    "url": "/static/js/2.7ec5e60e.chunk.js"
+  },
+  {
+    "revision": "249a4c26dd7edf964147",
+    "url": "/static/js/main.0b5eb814.chunk.js"
+  },
+  {
+    "revision": "30e0ed69445366f569d9",
+    "url": "/static/js/runtime-main.3c2d1558.js"
+  },
+  {
+    "revision": "9d65f4e610da59ad14754d800a3cc41b",
+    "url": "/static/media/Open-24-Display-St.9d65f4e6.ttf"
+  },
+  {
+    "revision": "346c3e47d1df1fcdb0602e4d77b97566",
+    "url": "/static/media/Yesteryear-Regular.346c3e47.ttf"
+  }
+]);
